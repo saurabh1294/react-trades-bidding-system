d316ad9b9107b44da8d3579a55eb6446c3b7e03e
Saurabh Gattani <saurabh.gattani@gmail.com>
2022-11-28 10:13:50 +1100

    put back latest commit files

--- backend/index.ts
+++ backend/index.ts
@@ -21,7 +21,7 @@ app.use(cors());
 
 const storage = multer.diskStorage({
   destination: function (req: any, file: any, cb: any) {
-    cb(null, "/Users/saurabhgattani/Projects/CIS_final/react-bidding-system/frontend/public/uploads");
+    cb(null, "/Users/saurabhgattani/Projects/intuit/react-trades-bidding-system/frontend/public/uploads");
   },
   filename: function (req: any, file: any, cb: any) {
     cb(null, Date.now() + "-" + file.originalname);
